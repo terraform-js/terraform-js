54dcc8144f67bc6998125f6e7e4af9e2114e2fad
Prakash Nath Jha <prakashecdrid@gmail.com>
2022-09-21 00:10:50 +0530

    feat: add apply and destroy actions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import _, { List } from "lodash";
 import execa from "execa";
 
 export type TerraformConfig = {
@@ -6,6 +6,8 @@ export type TerraformConfig = {
   workspace: string;
   templateDirectory?: string;
 };
+
+export type StdioHandler = "inherit" | "pipe";
 
 const defaultConfig: TerraformConfig = {
   logLevel: "INFO",
@@ -32,6 +34,49 @@ export class Terraformer {
       });
     }
   }
-  private initialize() {}
-  private async setup() {}
+  private async initialize() {}
+  async setup() {
+    await this.setWorkspace();
+    await this.initialize();
+  }
+  async apply() {
+    const options = ["apply", "-auto-approve"];
+    await this.execute(options, "inherit");
+  }
+  async destroy() {
+    const options = ["destroy", "-auto-approve"];
+    await this.execute(options, "inherit");
+  }
+  async output() {
+    const options = ["output", "-json"];
+    return (await this.execute(options, "pipe")).stdout;
+  }
+  async getState() {
+    const options = ["show", "-json"];
+    const state = await this.execute(options, "pipe");
+    return JSON.parse(state.stdout);
+  }
+  private async execute(parameters: Array<string>, stdioHandler: StdioHandler) {
+    const commandOpts = parameters.join(" ");
+    const logLevel = _.defaultTo(process.env.TF_LOG, this.logLevel);
+    const command = `terraform ${commandOpts}`;
+    try {
+      return await execa.execaCommand(command, {
+        stdio: stdioHandler,
+        shell: true,
+        env: {
+          TF_INPUT: "false",
+          TF_LOG: logLevel,
+          TF_IN_AUTOMATION: "true",
+          TF_CLI_ARGS_import: "-no-color",
+          TF_CLI_ARGS_apply: "-no-color",
+          TF_CLI_ARGS_init: "-no-color",
+          TF_CLI_ARGS_destroy: "-no-color"
+        }
+      });
+    } catch (err) {
+      console.log(`Failed to execute command: ${command} with error: `, err);
+      throw err;
+    }
+  }
 }
